4728061aa51a49a61a81ba269043304e7f2c76b8
Soulsaw <ssawadogo292@gmail.com>
2023-12-22 07:57:21 +0000

    first commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from . import app
+""" Importing the app instance for the running and define the route
+"""
+@app.route('/', strict_slashes=False)
+def hello():
+    """ This function display hello HBNH! at the route URL
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    """The entry point of the program
+    """
+    app.run(host='0.0.0.0', port=5005)
---
+++ web_flask/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from flask import Flask
+"""This is the flask instance that will be use in all the project
+"""
+app = Flask(__name__)
